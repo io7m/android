bbb24ce4829502779a5df5d44cf1de5180920bbc
Mark Raynsford <code@io7m.com>
2018-01-11 12:18:37 +0000

    Correct some unimplemented code

--- simplified-app-shared/src/main/java/org/nypl/simplified/app/HelpActivity.java
+++ simplified-app-shared/src/main/java/org/nypl/simplified/app/HelpActivity.java
@@ -3,19 +3,20 @@ package org.nypl.simplified.app;
 import android.os.Bundle;
 
 import com.io7m.jfunctional.OptionType;
-import com.io7m.jfunctional.ProcedureType;
 import com.io7m.jnull.Nullable;
-import com.io7m.junreachable.UnimplementedCodeException;
 
 /**
  * The activity that shows Helpstack's main activity
  */
+
 public final class HelpActivity extends NavigationDrawerActivity {
 
   /**
    * Construct help activity
    */
+
   public HelpActivity() {
+
   }
 
   @Override
@@ -33,18 +34,11 @@ public final class HelpActivity extends NavigationDrawerActivity {
       final @Nullable Bundle state) {
     super.onCreate(state);
 
-    final OptionType<HelpstackType> helpstack = getHelpStack();
-    helpstack.map_(
-        new ProcedureType<HelpstackType>() {
-          @Override
-          public void call(final HelpstackType hs) {
-            hs.show(HelpActivity.this);
-            HelpActivity.this.finish();
-          }
-        });
+    final OptionType<HelpstackType> helpstack = Simplified.getHelpStack();
+    helpstack.map_(hs -> {
+      hs.show(HelpActivity.this);
+      this.finish();
+    });
   }
 
-  private static OptionType<HelpstackType> getHelpStack() {
-    throw new UnimplementedCodeException();
-  }
 }
